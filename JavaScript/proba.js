e24e28ef9e903b00d512627ebf4e4d98ab8c6f51
[bigsee1970] <[dontwritetome70@gmail.com]>
2025-07-17 16:49:35 +0200

    JS...START1!

---
+++ JavaScript/proba.js
@@ -0,0 +1,17 @@
+let p = prompt("Mennyi idős vagy?")
+
+
+    if ( p<18) 
+{
+        alert("Mennyé haza!")
+
+}
+
+ else if (p>18)
+{
+    alert("Üdvözlünk a felnőttek világában!")
+}
+
+else {
+    alert("Üdvözlünk a felnőttek világában!")
+}
