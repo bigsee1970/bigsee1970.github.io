b5f9b0085cfbbef6ea487605a93e7b691d9f20d2
[bigsee1970] <[dontwritetome70@gmail.com]>
2025-07-17 18:04:34 +0200

    JS...START1!

--- JavaScript/Levai_HW_FOK.js
+++ JavaScript/Levai_HW_FOK.js
@@ -1,12 +1,12 @@
 let x = Number(prompt("Kérek egy számot (1-180)!"));
-let y = Number(prompt("Kérek egy másik számot (1-180)!"));
+let y = Number(prompt("Kérek egy második számot (1-180)!"));
 let z = Number(prompt("Kérek egy harmadik számot (1-180)!"));
 
 if (x + y + z == 180) {
-  console.log("Ez egy háromszög!");
+  alert("Ez egy háromszög!");
 } else if (x + y + z > 180) {
-  console.log("Ez nem egy háromszög! (belső fokok összege több, mint 180 fok!)");
+  alert("Ez nem egy háromszög! (belső fokok összege több, mint 180 fok!)");
 } else {
-  console.log("Ez nem egy háromszög! (belső fokok összege kevesebb, mint 180 fok!)");
+  alert("Ez nem egy háromszög! (belső fokok összege kevesebb, mint 180 fok!)");
 }
  
